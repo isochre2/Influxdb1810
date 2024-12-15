93671d536cb1b7fc599a69ca926b63e8e7f6e991
Isochre <isochre@gmail.com>
2024-12-15 22:17:09 +0100

    mise à jour du script d'initialisation de la bd

--- init-influxdb.sh
+++ init-influxdb.sh
@@ -16,8 +16,8 @@ fi
 
 # Set up retention policies
 influx -execute "CREATE RETENTION POLICY \"one_hour\" ON \"PowerMonitorDatabase\" DURATION 1h REPLICATION 1 DEFAULT"
-echo "Retention policy 'one_week' set on PowerMonitorDatabase."
+echo "Retention policy 'one_hour' set on PowerMonitorDatabase."
 
 # Set up continuous queries (adjust these to your requirements)
-# influx -execute "CREATE CONTINUOUS QUERY 'cq_example' ON 'PowerMonitorDatabase' BEGIN SELECT mean(value) AS avg_value INTO 'PowerMonitorDatabase'.'one_week'.'average_values' FROM 'measurement_name' GROUP BY time(1h) END"
-# echo "Continuous query 'cq_example' created."
+#influx -execute "CREATE CONTINUOUS QUERY \"cq_power\" ON 'PowerMonitorDatabase' BEGIN SELECT mean(\"Power\") AS avg_value INTO 'PowerMonitorDatabase'.'one_week'.'average_values' FROM 'measurement_name' GROUP BY time(1h) END"
+#echo "Continuous query 'cq_example' created."
