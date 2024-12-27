7cfafe6f725edfafc1e6779b45b30a5e10860563
Isochre <isochre@gmail.com>
2024-12-28 00:31:24 +0100

    mise à jour du script de démarrage

--- init-influxdb.sh
+++ init-influxdb.sh
@@ -19,5 +19,11 @@ influx -execute "CREATE RETENTION POLICY \"one_hour\" ON \"PowerMonitorDatabase\
 echo "Retention policy 'one_hour' set on PowerMonitorDatabase."
 
 # Set up continuous queries (adjust these to your requirements)
-#influx -execute "CREATE CONTINUOUS QUERY \"cq_power\" ON 'PowerMonitorDatabase' BEGIN SELECT mean(\"Power\") AS avg_value INTO 'PowerMonitorDatabase'.'one_week'.'average_values' FROM 'measurement_name' GROUP BY time(1h) END"
-#echo "Continuous query 'cq_example' created."
+influx -execute "USE \"PowerMonitorDatabase\""
+influx -execute "CREATE CONTINUOUS QUERY \"cq_energy_hourUV\" ON \"PowerMonitorDatabase\" RESAMPLE EVERY 1s BEGIN SELECT INTEGRAL(\"Power\", 1h) AS \"LineLastHourEnergy_UV\" INTO \"PowerMonitorDatabase\".\"autogen\".\"EnergyMeasurement\" FROM \"Power\" WHERE \"Line\" = 'UV' GROUP BY time(1h) END"
+influx -execute "CREATE CONTINUOUS QUERY \"cq_energy_hourVW\" ON \"PowerMonitorDatabase\" RESAMPLE EVERY 1s BEGIN SELECT INTEGRAL(\"Power\", 1h) AS \"LineLastHourEnergy_VW\" INTO \"PowerMonitorDatabase\".\"autogen\".\"EnergyMeasurement\" FROM \"Power\" WHERE \"Line\" = 'VW' GROUP BY time(1h) END"
+influx -execute "CREATE CONTINUOUS QUERY \"cq_energy_hourWU\" ON \"PowerMonitorDatabase\" RESAMPLE EVERY 1s BEGIN SELECT INTEGRAL(\"Power\", 1h) AS \"LineLastHourEnergy_WU\" INTO \"PowerMonitorDatabase\".\"autogen\".\"EnergyMeasurement\" FROM \"Power\" WHERE \"Line\" = 'WU' GROUP BY time(1h) END"
+echo "Continuous query 'cq_energy' created."
+
+#Commande pour accéder à influxQL
+#influx -precision rfc3339
